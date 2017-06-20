47f7a03af09405a2529598820150478fba1935d6
Antony Rony <antony.rony@mobme.in>
2017-06-20 12:33:21 +0530

    changed forceupdate class

--- lib/forceupdate.rb
+++ lib/forceupdate.rb
@@ -2,10 +2,12 @@ require "forceupdate/version"
 require_relative 'api_exception'
 
 # force update class
-class Forceupdate
+module Forceupdate
+  class << self
   # check app os and app version
-  def app_update_check(os, app_version)
-    raise SkipUpdateError if UPDATE_CONFIG['skip_update'][os].values.include?(app_version)
-    raise ForceUpdateError if UPDATE_CONFIG['force_update'][os].values.include?(app_version)
+    def app_update_check(os, app_version)
+      raise SkipUpdateError if UPDATE_CONFIG['skip_update'][os].values.include?(app_version)
+      raise ForceUpdateError if UPDATE_CONFIG['force_update'][os].values.include?(app_version)
+    end
   end
 end
